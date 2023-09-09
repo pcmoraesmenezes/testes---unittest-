c664b1358dce32cb268e08ccdf7014b95ee60d04
pcmoraesmenezes <paulo.moraes@sou.unifal-mg.edu.br>
2023-09-09 06:28:31 -0300

    Adding a branch for clean-code notes

--- test_bacon_with_eggs.py
+++ test_bacon_with_eggs.py
@@ -8,42 +8,42 @@ from bacon_with_eggs import bacon_with_eggs
 import unittest
 
 
+def for_in_sub_test(entrys, output):
+    for_test = unittest.TestCase()
+    for entry in entrys:
+        with for_test.subTest(entry=entry, output=output):
+            for_test.assertAlmostEqual(bacon_with_eggs(entry), output)
+
+
+def entry_is_string():
+    entry_string = unittest.TestCase()
+    with entry_string.assertRaises(AssertionError):
+        bacon_with_eggs('')
+
+
 class TestBaconWithEggs(unittest.TestCase):
     def test_bacon_with_eggs_must_raise_assertion_error_if_not_recieve_int(self):  # noqa
-        with self.assertRaises(AssertionError):
-            bacon_with_eggs('')
+        entry_is_string()
 
     def test_bacon_with_eggs_must_return_bacon_with_eggs_if_entry_is_multiple_3_and_5(self):  # noqa
         entrys = (15, 30, 45, 60)
         output = 'Bacon with eggs'
-
-        for entry in entrys:
-            with self.subTest(entry=entry, output=output):
-                self.assertEqual(bacon_with_eggs(entry), output)
+        for_in_sub_test(entrys, output)
 
     def test_bacon_with_eggs_must_return_hungry_if_is_not_multiple_3_and_5(self):  # noqa
         entrys = (1, 2, 4, 11, 7, 8)
         output = 'Hungry'
-
-        for entry in entrys:
-            with self.subTest(entry=entry, output=output):
-                self.assertEqual(bacon_with_eggs(entry), output)
+        for_in_sub_test(entrys, output)
 
     def test_bacon_with_eggs_must_return_bacon_if_is_multiple_3(self):  # noqa
         entrys = (3, 6, 9, 12, 18, 21)
         output = 'Bacon'
-
-        for entry in entrys:
-            with self.subTest(entry=entry, output=output):
-                self.assertEqual(bacon_with_eggs(entry), output)
+        for_in_sub_test(entrys, output)
 
     def test_bacon_with_eggs_must_return_eggs_if_is_multiple_5(self):  # noqa
         entrys = (5, 10, 20, 25, 35, 40)
         output = 'Eggs'
-
-        for entry in entrys:
-            with self.subTest(entry=entry, output=output):
-                self.assertEqual(bacon_with_eggs(entry), output)
+        for_in_sub_test(entrys, output)
 
 
 unittest.main(verbosity=2)
